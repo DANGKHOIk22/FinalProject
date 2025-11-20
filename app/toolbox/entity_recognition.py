764d9fe5d40c16252b3fb896fc1fcff42d4254e3
DANGKHOIk22 <Khoivudang03@gmail.com>
2025-11-20 18:09:36 +0700

    fix: update DNS resolver import and gitignore

--- app/toolbox/entity_recognition.py
+++ app/toolbox/entity_recognition.py
@@ -2,7 +2,7 @@ import os
 import logging
 from typing import List, Optional, Dict, Tuple, Annotated
 
-import dns.resolver
+from dns import resolver
 import pymongo
 from pymongo.server_api import ServerApi
 
@@ -124,8 +124,8 @@ def query_database(soccer_entities: list) -> SearchingResult:
             return result
 
         # Connect to MongoDB
-        dns.resolver.default_resolver = dns.resolver.Resolver(configure=False)
-        dns.resolver.default_resolver.nameservers = ['8.8.8.8', '1.1.1.1']  
+        resolver.default_resolver = resolver.Resolver(configure=False)
+        resolver.default_resolver.nameservers = ['8.8.8.8', '1.1.1.1']  
         client = pymongo.MongoClient(mongo_srv, server_api=ServerApi('1'))
         db = client.get_database(name=database_name)
         collection = db.get_collection(name=collection_name)
