5e92aa8082949457eca4bffa10dc86599a31c338
DANGKHOIk22 <Khoivudang03@gmail.com>
2026-06-11 01:51:19 +0700

    Refactor router registrations and remove unused dependencies
    - Updated main.py to include upload_router for handling uploads.
    - Removed unused packages from uv.lock.

--- main.py
+++ main.py
@@ -209,7 +209,7 @@ app.add_middleware(
 # --- 4. Đăng ký Router ---
 app.include_router(chat_router, tags=["Soccer Chat Agent"])
 app.include_router(user_router, prefix="/user", tags=["User"])
-app.include_router(hls_router, tags=["HLS Streaming"])
+app.include_router(upload_router,  tags=["Upload"])
 
 
 
